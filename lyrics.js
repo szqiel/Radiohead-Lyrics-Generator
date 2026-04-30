bb8ab1c4749760b38b9732168ae708112ad3ccc0
szqiel <193689742+szqiel@users.noreply.github.com>
2026-04-30 11:15:07 +0700

    Added More Lyrics,Songs,Albums

--- lyrics.js
+++ lyrics.js
@@ -23,6 +23,31 @@ const RADIOHEAD_LYRICS = {
         "I'd show them the stars and the meaning of life They'd shut me away, but I'd be alright",
         "Alright, I'm alright"
       ]
+    },
+    {
+      "song": "Present Tense",
+      "album": "Moon Shaped Pool",
+      "cover": "assets/albums/Moon Shaped Pool.png",
+      "lines": [
+        "No, I won't get heavy, don't get heavy",
+        "As my world comes crashing down I'll be dancing, freaking out",
+        "In you, I'm lost",
+        "I won't turn around or the penny drops, Won't stop now, won't slack off",
+        "To stop from falling down a mine It's no one's business but mine"
+      ]
+    },
+    {
+      "song": "Creep",
+      "album": "Pablo Honey",
+      "cover": "assets/albums/Pablo Honey.png",
+      "lines": [
+        "You're just like an angel",
+        "What the hell am I doing here?",
+        "I don't belong here",
+        "I wish I was special",
+        "I don't care if it hurts",
+        "I want you to notice when I'm not around",
+      ]
     }
   ]
 };
